bdf62da2a55ff6c436f5838629e6e78e9f1d358e
Anton Khomenko <xomafox8@gmail.com>
2023-03-08 13:09:21 +0200

    add HD class

---
+++ ShopProject/HR.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject
+{
+    internal class HR
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public HR (string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return String.Format(ID + " " + FirstName + " " + LastName + " " + Age);
+        }
+    }
+}
