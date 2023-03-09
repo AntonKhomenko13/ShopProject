4fb4246e914d6dc5572c0a1bb98a1c1adb4de2b2
sulky <sulky1230@gmail.com>
2023-03-09 12:46:47 +0300

    Додано класи SalesManager та Client

--- ShopProject/Person.cs
+++ ShopProject/Person.cs
@@ -1,19 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShopProject
+﻿namespace ShopProject
 {
     internal class Person
     {
         public string FirstName { get; private set; }
-
         public string LastName { get; private set; }
-
         public int Age { get; private set; }
-
         public Person(string firstName, string lastName, int age)
         {
             FirstName = firstName;
--- ShopProject/ProductManager.cs
+++ ShopProject/ProductManager.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ShopProject
+﻿namespace ShopProject
 {
     internal class ProductManager : Person
     {
         public int ID { get; private set; }
         public string FirstName { get; private set; }
-
         public string LastName { get; private set; }
-
         public int Age { get; private set; }
 
         public ProductManager(string firstName, string lastName, int age) : base(firstName, lastName, age)
