ddabe50fddb3b80c13df372a547a5c1b8c5fcaf8
Максим Наумов <maximnaumov404@gmail.com>
2023-03-09 10:09:29 +0200

    Додано клас ProductCategory

---
+++ ShopProject/ProductCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject
+{
+    internal class ProductCategory
+    {
+        public int ID { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public ProductCategory(int productID, int categoryID)
+        {
+            ProductID = productID;
+            CategoryID = categoryID;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(ID + " " + ProductID + " " + CategoryID);
+        }
+    }
+}
