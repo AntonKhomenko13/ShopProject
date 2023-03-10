0df5ec1931de709a55dde00e6e13271565677dbb
AntonKhomenko13 <73781794+AntonKhomenko13@users.noreply.github.com>
2023-03-10 18:15:37 +0200

    add class1 (#7)
    * add HD class
    
    * Change set to private
    
    * Додано клас Person та організовано успадкування від Person до PM, HR
    
    * add class1
    
    * add class1

--- ShopProject/Person.cs
+++ ShopProject/Person.cs
@@ -1,10 +1,19 @@
-﻿namespace ShopProject
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject
 {
     internal class Person
     {
         public string FirstName { get; private set; }
+
         public string LastName { get; private set; }
+
         public int Age { get; private set; }
+
         public Person(string firstName, string lastName, int age)
         {
             FirstName = firstName;
