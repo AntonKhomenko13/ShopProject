d5b3269f2927d97a6df33ea7b915b2604d71fc26
Vitalii <sulky1230@gmail.com>
2023-03-13 17:20:55 +0300

    Створено метод CreateProduct і конструктор в класі ProductPL

--- ShopProject/ProductPL.cs
+++ ShopProject/ProductPL.cs
@@ -2,5 +2,16 @@
 {
     internal class ProductPL
     {
+        ProductBLL productBLL;
+        public ProductPL(ProductBLL productBLL)
+        {
+            this.productBLL = productBLL;
+        }
+        public void CreateProduct()
+        {
+            Console.Write("CreateProduct productName?: ");
+            string productName = Console.ReadLine();
+            productBLL.CreateProduct(productName);
+        }
     }
 }
