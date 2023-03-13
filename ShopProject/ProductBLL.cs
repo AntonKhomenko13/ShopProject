80bbd544e8abdfe9ec0a081e121766ef4d52621a
Vitalii <sulky1230@gmail.com>
2023-03-13 17:12:25 +0300

    Добавлено метод і конструктор до класу ProductBLL

--- ShopProject/ProductBLL.cs
+++ ShopProject/ProductBLL.cs
@@ -2,5 +2,18 @@
 {
     internal class ProductBLL
     {
+        DB db;
+
+        public ProductBLL(DB db)
+        {
+            this.db = db;
+        }
+
+        public void CreateProduct(string productName)
+        {
+            Product product = new Product(productName);
+            DBItem<Product> dbProduct = db.DBProduct;
+            dbProduct.AddItem(product);
+        }
     }
 }
