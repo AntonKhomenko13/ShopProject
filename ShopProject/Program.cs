67cc2c5e737eb85ca1645d6e31d281df2cc6db4b
MaksMarenko <marenko9921@gmail.com>
2023-03-14 10:34:10 +0200

    Додано діаграму станів

--- ShopProject/Program.cs
+++ ShopProject/Program.cs
@@ -1,8 +1,2 @@
-﻿using ShopProject;
-
-ProductManager productManager = new ProductManager("Anton", "Khomenko", 20);
-Console.WriteLine(productManager);
-
-HR HR = new HR("Anna", "Plahotnik", 21);
-Console.WriteLine(HR);
-
+﻿
+Console.WriteLine("1");
