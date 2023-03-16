07891609d977f4fb1e777b2b9826bf0aac43d55d
Artur Lisetskyi <funnyarthur183@gmail.com>
2023-03-16 10:38:11 +0200

    Додано AbstractMenu

---
+++ ShopProject/AbstractMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ShopProject
+{
+    internal abstract class AbstractMenu
+    {
+        protected bool Flag { get; set; } = true;
+
+        public void Run()
+        {
+            Init();
+            while (Flag == true)
+            {
+                Idle();
+            }
+            CleanUp();
+        }
+        protected abstract void CleanUp();
+        protected abstract void Init();
+        protected abstract void Idle();
+    }
+}
+
+
