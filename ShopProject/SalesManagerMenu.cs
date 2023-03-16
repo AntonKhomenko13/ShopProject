c484a614355c4e1f743eeae68103c19909036a21
Vitalii <sulky1230@gmail.com>
2023-03-16 23:30:16 +0200

    В класі SalesManagerMenu було написано реалізацію

--- ShopProject/SalesManagerMenu.cs
+++ ShopProject/SalesManagerMenu.cs
@@ -1,7 +1,43 @@
 ﻿namespace ShopProject
 {
-    internal class SalesManagerMenu
+    internal class SalesManagerMenu : AbstractMenu
     {
-        
+        ConsoleColor defaultColor;
+        protected override void Init()
+        {
+            defaultColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        protected override void Idle()
+        {
+            Console.WriteLine("1 - Client CRUD Menu");
+            Console.WriteLine("2 - Order CRUD Menu");
+            Console.WriteLine("3 - Sales Manager Services Menu");
+            Console.WriteLine("4 - Exit");
+
+            int menuNumber = int.Parse(Console.ReadLine());
+            switch (menuNumber)
+            {
+                case 1:
+                    Console.WriteLine("Client CRUD Menu");
+                    break;
+                case 2:
+                    Console.WriteLine("Order CRUD Menu");
+                    break;
+                case 3:
+                    Console.WriteLine("Sales Manager Services Menu");
+                    break;
+                case 4:
+                    Flag = false;
+                    break;
+                case 5:
+                    Console.WriteLine("Input the right number!");
+                    break;
+            }
+        }
+        protected override void CleanUp()
+        {
+            Console.ForegroundColor = defaultColor;
+        }
     }
 }
