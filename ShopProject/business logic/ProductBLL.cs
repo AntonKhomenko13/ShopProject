65eb339e7cfa8a80ed619d27dacca15b13beff5f
Anton Khomenko <xomafox8@gmail.com>
2023-03-21 11:08:39 +0200

    New change in ProductBLL class

--- ShopProject/business logic/ProductBLL.cs
+++ ShopProject/business logic/ProductBLL.cs
@@ -18,8 +18,25 @@
         public Product GetByID(int id)
         {
             Product result = default(Product);
-            db.DBProduct
-            return null;
+            result = db.DBProduct.GetById(id);
+            return result;
+        }
+        public List<Product> GetAllProducts()
+        {
+            return db.DBProduct.Items;
+        }
+        public bool UpDateProduct(Product oldItem, string productName)
+        {
+            bool result = false;
+            Product newItem = new Product(productName);
+            result = db.DBProduct.Update(oldItem, newItem);
+            return result;
+        }
+        public bool DeleteProduct(Product product)
+        {
+            bool result = false;
+            result = db.DBProduct.Delete(product);
+            return result;
         }
     }
 }
