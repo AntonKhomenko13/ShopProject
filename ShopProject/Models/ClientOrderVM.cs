cdc0de38d44285f7de2e9101dcf0268fe9f8d9bb
Anton Khomenko <xomafox8@gmail.com>
2023-03-23 16:50:38 +0200

    Created and filled the ClientOrderVM class

---
+++ ShopProject/Models/ClientOrderVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject
+{
+    internal class ClientOrderVM
+    {
+        public string ClientFirstName { get; set; }        
+        public string ClientLastName { get; set; }
+        public string OrderName { get; set; }
+        public double Sum { get; set; }
+
+        public ClientOrderVM(string clientFirstName, string clientLastName, string orderName, double sum)
+        {
+            ClientFirstName = clientFirstName;
+            ClientLastName = clientLastName;
+            OrderName = orderName;
+            Sum = sum;
+        }
+        public override string ToString()
+        {
+            return string.Format(ClientFirstName + " " + ClientLastName + " " + OrderName + " " + Sum);
+        }
+    }
+}
