f7b64bee58d552b023ee3df8d7f9840c13e5e585
Anton Khomenko <xomafox8@gmail.com>
2023-03-23 17:02:15 +0200

    The SMServivesPL class is created and populated

---
+++ ShopProject/presentation layer/SMServivesPL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopProject
+{
+    internal class SMServivesPL
+    {
+        SMServicesBLL smServicesBLL;
+
+        public SMServivesPL(SMServicesBLL smServicesBLL)
+        {
+            this.smServicesBLL = smServicesBLL;
+        }
+        public void GetClientOrders()
+        {
+            List<ClientOrderVM> clientOrders = smServicesBLL.GetClientOrders();
+            foreach (ClientOrderVM clientOrder in clientOrders)
+            {
+                Console.WriteLine(clientOrder);
+            }
+        }
+        public void GetClientSum()
+        {
+            List<ClientSumVm> clientSums = smServicesBLL.GetClientSum();
+            foreach (ClientSumVm clientSumVm in clientSums)
+            {
+                Console.WriteLine(clientSumVm);
+            }
+        }
+    }
+}
