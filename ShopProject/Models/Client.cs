e4d95fa0e3a500fb4456143c5eb9eb251af6bf67
Anton Khomenko <xomafox8@gmail.com>
2023-03-23 16:57:25 +0200

    Updated the Client class

--- ShopProject/Models/Client.cs
+++ ShopProject/Models/Client.cs
@@ -3,14 +3,11 @@
     internal class Client : Person, IID
     {
         public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
 
         public Client(string firstName, string lastName, int age) : base(firstName, lastName, age) { }
         public override string ToString()
         {
-            return String.Format($"{ID} {base.ToString()}");
+            return string.Format($"{ID} {base.ToString()}");
         }
     }
 }
