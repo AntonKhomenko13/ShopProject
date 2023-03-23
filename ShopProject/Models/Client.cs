51dd00dc2529b44e354216e085f355d3dd514a63
Anton Khomenko <xomafox8@gmail.com>
2023-03-23 16:58:49 +0200

    Revert "Updated the Client class"
    This reverts commit 4307134d47f1c597358105b85829bd9aa7dfa5c1.

--- ShopProject/Models/Client.cs
+++ ShopProject/Models/Client.cs
@@ -3,6 +3,7 @@
     internal class Client : Person, IID
     {
         public int ID { get; set; }
+
         public Client(string firstName, string lastName, int age) : base(firstName, lastName, age) { }
         public override string ToString()
         {
